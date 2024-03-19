0d32cf8c27c0b72b93f0b710113239ce2579c012
ymc9 <104139426+ymc9@users.noreply.github.com>
2024-03-19 13:39:32 -0700

    fix

--- packages/@erikdakoda/database/index.ts
+++ packages/@erikdakoda/database/index.ts
@@ -1,5 +1,5 @@
-import { Prisma, $Enums } from '@prisma/client';
-export * from '@prisma/client';
+import { Prisma, $Enums } from './prisma/client';
+export * from './prisma/client';
 export const Enums = $Enums;
 export const { SortOrder, prismaVersion, DbNull, JsonNull, AnyNull, ModelName, NullsOrder } =
   Prisma;
